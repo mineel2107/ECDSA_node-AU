baee043afad6ecac15b06369d9d1dd7f1ee04cdf
Mineel <mineelbunty2002@gmail.com>
2023-10-02 15:47:33 +0530

    Authentication implemented for sender using Digital SIgnature

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import server from "./server";
+import { secp256k1 } from "ethereum-cryptography/secp256k1";
+import { utf8ToBytes } from "ethereum-cryptography/utils";
+// import { sha256 } from "ethereum-cryptography/sha256";
 
 function Transfer({ address, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
 
   async function transfer(evt) {
     evt.preventDefault();
+
+    const msgHash = sha256(utf8ToBytes(sendAmount));
+    const signature = secp256k1.sign(msgHash, privateKey);
 
     try {
       const {
@@ -17,6 +24,7 @@ function Transfer({ address, setBalance }) {
         sender: address,
         amount: parseInt(sendAmount),
         recipient,
+        signature,
       });
       setBalance(balance);
     } catch (ex) {
@@ -43,6 +51,14 @@ function Transfer({ address, setBalance }) {
           placeholder="Type an address, for example: 0x2"
           value={recipient}
           onChange={setValue(setRecipient)}
+        ></input>
+      </label>
+      <label>
+        Provide Your Private Key To Sign The Trasaction In A Secure Way
+        <input
+          placeholder="Type an address, for example0 35sda54sda..."
+          value={privateKey}
+          onChange={setValue(setPrivateKey)}
         ></input>
       </label>
 
--- server/index.js
+++ server/index.js
@@ -2,14 +2,23 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+const { secp256k1 } = require("ethereum-cryptography/secp256k1");
 
 app.use(cors());
 app.use(express.json());
 
+// Private Key : b36d66e11eb2a2936b235cc776fa613743892a25ba5f00510f081ea54f39b838
+// Public Key : 02faddcfeb18599f00f09b49ad36edeb2df746f3d763ce0dc1cb5672688999b191
+
+// Private Key : 021a07ac74b17f7754aac7423411b2d345774f80331e34307d64fe9cc0e0e2dc
+// Public Key : 02d964cd3dc074451f2cab3bb7b294360c8cd5859caacebf76e1eb493f7b6b745f
+
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "02faddcfeb18599f00f09b49ad36edeb2df746f3d763ce0dc1cb5672688999b191": 100,
+  "02d964cd3dc074451f2cab3bb7b294360c8cd5859caacebf76e1eb493f7b6b745f": 50,
+  "032a62842c9ae5a42dfcae33e3ffddeb100985e673d6e9cd6bf8340f5285d93e1a": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,17 +28,29 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  //TODO: get a signature from client-side application
+  // recover the public address from the signature
+
+  const { sender, recipient, amount, sign } = req.body;
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
+  signature.r = BigInt(signature.r);
+  signature.s = BigInt(signature.s);
+
+  const msgHash = sha256(utf8ToBytes(amount.toString()));
+
+  if (secp256k1.verify(signature, msgHash, sender)) {
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
   } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+    res.status(400).send({ message: "Authentication Failed" });
   }
 });
 
